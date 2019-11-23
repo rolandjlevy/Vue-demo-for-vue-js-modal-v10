765660c76325d0586b9a87ae89aa5169c5125ad1
Roland  Levy <rolandjlevy@gmail.com>
2019-11-23 21:22:30 +0000

    Changed order of modal buttons

--- js/apps.js
+++ js/apps.js
@@ -35,14 +35,14 @@ const tasksApp = new Vue({
       const { name, header } = options;
       this.log += this.getMessageLog(header);
     });
-    eventBus.$on('measure-save-prompt', (options) => {
-      const { name, header, payload } = options;
-      const msg = `${header}. User input: '${payload.inputvalue}'`
-      this.log += this.getMessageLog(msg);
-    });
     eventBus.$on('table-modal', (options) => {
       const { name, header, payload } = options;
       const msg = `${header}. User input: [${payload.groupvalue}]`
+      this.log += this.getMessageLog(msg);
+    });
+    eventBus.$on('save-measure-prompt', (options) => {
+      const { name, header, payload } = options;
+      const msg = `${header}. User input: '${payload.inputvalue}'`
       this.log += this.getMessageLog(msg);
     });
   }
